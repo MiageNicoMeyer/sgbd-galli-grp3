3a7406dc82cbd5aa34afd5394ddd15a9c40e2660
Nicolas Meyer <hatari@hatarax.com>
2021-06-27 22:32:02 +0200

    Algorithme de recherche - v1

--- src/fr/miage/fsgbd/BTreePlus.java
+++ src/fr/miage/fsgbd/BTreePlus.java
@@ -1,13 +1,15 @@
 package fr.miage.fsgbd;
 
 import javax.swing.tree.DefaultMutableTreeNode;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 
 /**
  * @author Galli Gregory, Mopolo Moke Gabriel
  * @param <Type>
  */
-public class BTreePlus<Type> implements java.io.Serializable {
+public class BTreePlus<Type> implements Serializable {
     private Noeud<Type> racine;
 
     public BTreePlus(int u, Executable e) {
@@ -41,7 +43,6 @@ public class BTreePlus<Type> implements java.io.Serializable {
 
 
     public boolean addValeur(Type valeur) {
-        System.out.println("Ajout de la valeur : " + valeur.toString());
         if (racine.contient(valeur) == null) {
             Noeud<Type> newRacine = racine.addValeur(valeur);
             if (racine != newRacine)
@@ -59,5 +60,60 @@ public class BTreePlus<Type> implements java.io.Serializable {
             if (racine != newRacine)
                 racine = newRacine;
         }
+    }
+
+    public Personne searchIndex(Type index,Noeud<Type> arbre) {
+
+        if(arbre.fils.size()==0) {
+            if(arbre.keys.contains(index)) {
+                for (Type key : arbre.keys) {
+                    if ((int) key == (int) index) {
+                        Personne result = cherchePersonne(arbre.p, index);
+                        if(result != null) return result;
+                    }
+                }
+            }
+            return null;
+        }
+        else {
+            for (Noeud<Type> n : arbre.fils) {
+                Personne result = searchIndex(index, n);
+                if(result != null) return result;
+            }
+            return null;
+        }
+    }
+
+    private Personne cherchePersonne(ArrayList<Personne> p, Type index) {
+        for(Personne pers : p){
+            if(pers.key==(int)index){
+                return pers;
+            }
+        }
+        return null;
+    }
+
+
+    public Personne searchSeq(Type index,Noeud<Type> arbre) {
+
+        if(arbre.fils.size() != 0) {
+            for (Noeud<Type> n : arbre.fils) {
+                 Personne result = searchSeq(index, n);
+                 if(result != null) return result;
+            }
+            return null;
+        }
+        else{
+            if(arbre.keys.contains(index)) {
+                for(Personne p : arbre.p) {
+                    if(p.key == (int) index) return p;
+                }
+            }
+            return null;
+        }
+    }
+
+    public Noeud<Type> getRacine() {
+        return racine;
     }
 }
